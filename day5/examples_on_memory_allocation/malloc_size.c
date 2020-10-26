2addcbf29070a037b2ebf955dff26a7f1444b70a
lucatornatore <luca.tornatore@inaf.it>
2020-10-26 22:41:26 +0100

    fixed a typo; now that should work on MAC too

--- day5/examples_on_memory_allocation/malloc_size.c
+++ day5/examples_on_memory_allocation/malloc_size.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,9 +7,11 @@
 #include "benchmark.h"
 #ifdef __APPLE__
 #include <malloc/malloc.h>
+#define USABLE_SIZE(size) malloc_size((size))
 #endif
 #ifdef __linux__
 #include <malloc.h>
+#define USABLE_SIZE(size) malloc_usable_size((size))
 #endif 
  
 void demo( size_t N )
@@ -17,32 +20,23 @@ void demo( size_t N )
   int repeat        = 500;
   uint64_t * values = (uint64_t *) malloc(N * sizeof(uint64_t));
     
-#ifdef __APPLE__
-  
-    size_t expected = malloc_size(values);
-    printf("allocated %zu, reported allocation = %zu \n", N * sizeof(uint64_t), expected);
-    BEST_TIME(malloc_size(values), expected, , repeat, 1, true);
-    
-#elif defined(__linux__)
-    
-    size_t expected = malloc_usable_size(values);
-    printf("allocated %zu, reported allocation = %zu \n", N * sizeof(uint64_t), expected);
-    BEST_TIME(malloc_usable_size(values), expected, , repeat, 1, true);
-    
-#endif
-    
-    free(values);
-    
+  size_t expected = USABLE_SIZE(values);
+  printf("allocated %zu, reported allocation = %zu \n", N * sizeof(uint64_t), expected);
+  BEST_TIME(USABLE_SIZE(values), expected, , repeat, 1, true);
+      
+  free(values);
+  return;  
 }
 
 
 int main( void )
 {
-  printf("reported usabe_size for 1B allocated: %zu \n", malloc_usable_size(malloc(1)));
-  printf("reported usabe_size for 4B allocated: %zu \n", malloc_usable_size(malloc(4)));
-  printf("reported usabe_size for 8B allocated: %zu \n", malloc_usable_size(malloc(8)));
-  printf("reported usabe_size for 16B allocated: %zu \n", malloc_usable_size(malloc(16)));
-  printf("reported usabe_size for 32B allocated: %zu \n", malloc_usable_size(malloc(32)));
+  
+  printf("reported usabe_size for 1B allocated: %zu \n", USABLE_SIZE(malloc(1)));
+  printf("reported usabe_size for 4B allocated: %zu \n", USABLE_SIZE(malloc(4)));
+  printf("reported usabe_size for 8B allocated: %zu \n", USABLE_SIZE(malloc(8)));
+  printf("reported usabe_size for 16B allocated: %zu \n", USABLE_SIZE(malloc(16)));
+  printf("reported usabe_size for 32B allocated: %zu \n", USABLE_SIZE(malloc(32)));
   
   for( size_t N = 10; N < 100000000; N *= 10 )
     demo(N);
